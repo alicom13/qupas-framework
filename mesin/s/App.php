3f896d9372eb65736ef9057b9793cd6b552d539f
Ali Musthofa <alfaonline13@gmail.com>
2021-05-12 18:49:42 +0700

    Add files via upload

---
+++ mesin/s/App.php
@@ -0,0 +1,45 @@
+<?php
+
+class App {
+	protected $controller = 'Home';
+	protected $method = 'index';
+	protected $params = [];
+	public function __construct()
+	{
+		$url = $this->parseURL();
+		// Cek controller
+		if( file_exists('../mesin/c/' . $url[0] . '.php') ){
+			$this->controller = $url[0];
+			unset($url[0]);
+			//var_dump($url);
+		}
+		// Method
+		require_once '../mesin/c/' . $this->controller . '.php';
+		$this->controller = new $this->controller;
+		
+		if( isset($url[1]) ) {
+			if( method_exists($this->controller, $url[1]) ) {
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+		
+		// Parameter
+		if( !empty($url) ) {
+			$this->params = array_values($url);
+		}
+		// Jalankan controller & method, serta kirimkan params jika ada
+		call_user_func_array([$this->controller, $this->method], $this->params);
+		
+	}
+	
+	public function parseURL()
+	{
+		if( isset($_GET['url']) ){
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			return $url;
+		}
+	}
+}
