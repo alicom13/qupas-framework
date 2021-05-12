a03de149d5004e2f07a260ed92027738a3d8f49c
Ali Musthofa <alfaonline13@gmail.com>
2021-05-12 18:44:50 +0700

    Add files via upload

---
+++ mesin/sumber.php
@@ -0,0 +1,11 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|	Huruf S : folder System
+|	App.php : Class
+|	Controller.php : Controller
+*/
+require_once 's/App.php';
+require_once 's/Controller.php';
